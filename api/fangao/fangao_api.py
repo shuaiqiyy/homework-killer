8a11ba767ad4228ce5e23a9e2006c53f1aa6c5d5
shuaiqiyy <2592402521@qq.com>
2024-10-02 12:36:14 +0800

    :tada:更新api

--- api/fangao/fangao_api.py
+++ api/fangao/fangao_api.py
@@ -24,13 +24,13 @@ def user_infotmance(user_number,user_password):
     da_lodin = json.loads(r_login.text)
     code = da_lodin['code']
     if code == 0:
-        code = 200
+        code = 0
         token = da_lodin['data']['token']
         uid = da_lodin['data']['mid']
         name = da_lodin['data']['username']
         return code,token,uid,name
     else:
-        code = 401
+        code = 1
         token = mid = name = None
         return code,token,mid,name
 
@@ -49,6 +49,7 @@ def class_infomance(token,uid):
     da_class = json.loads(r_class.text)
     code = da_class['code']
     if code == 0:
+        code = 0
         class_data = da_class['data']
         for class_umb in class_data:
             class_name_list.append(f"{class_umb['class_name']},{class_umb['subject_name']}")
@@ -56,7 +57,7 @@ def class_infomance(token,uid):
             class_subject_list.append(class_umb['subject_id'])
         return code,class_name_list,class_id_list,class_subject_list
     else:
-        code = 401
+        code = 2
         class_name_list = class_id_list = class_subject_list = None
         return code,class_name_list,class_id_list
 
@@ -74,11 +75,36 @@ def homework_list_infomance(token,uid,class_id,subject_id):
     da_homework_list = json.loads(r_homework_list.text)
     code = da_homework_list['code']
     if code == 0:
+        code = 0
         for homework_umb in da_homework_list['data']:
             homework_list_name_list.append(homework_umb['homework_title'])
             homework_list_hid_list.append(homework_umb['hid'])
         return code,homework_list_name_list,homework_list_hid_list
     else:
-        code = 404
+        code = 3
         homework_list_name_list = homework_list_hid_list = None
         return code,homework_list_name_list,homework_list_hid_list
+
+def student_list_iformance(token,uid,hid,class_id):
+    hear = token_hear(token)
+    url_student_list = data_json['url_student_list']
+    student_list_name_liat = []
+    student_list_id_list = []
+    data_student_list = {
+    'request': '{{"mid":{uid},"type":2,"class_id":{classid},"hid":{hid}}}'.format(
+        uid=uid,classid=class_id,hid=hid)
+    }
+    r_student_list = requests.post(url_student_list,data=data_student_list, headers=hear, verify=False)
+    da_student_list = json.loads(r_student_list.text)
+    code = da_student_list['code']
+    da_student_list = da_student_list['data']
+    if code == 0:
+        code = 0
+        for  student_umb in da_student_list['student_list']:
+            student_list_name_liat.append(student_umb['username'])
+            student_list_id_list.append(student_umb['mid'])
+        return code,student_list_name_liat,student_list_id_list
+    else:
+        code = 4
+        student_list_name_liat = student_list_id_list = None
+        return code,student_list_name_liat,student_list_id_list
--- api/xiaoxin/xiaoxin_api.py
+++ api/xiaoxin/xiaoxin_api.py
@@ -16,13 +16,13 @@ def user_infotmance(user_number,user_password):
     da_login = json.loads(r_login.text)
     msg = da_login['state']
     if msg == 'ok':
-        code = 200
+        code = 0
         token = da_login['data']['token']
         uid = da_login['data']['userId']
         name = da_login['data']['realName']
         return code,token,uid,name
     else:
-        code = 401
+        code = 1
         token = uid = name = None
         return code,token,uid,name
 
@@ -39,6 +39,7 @@ def class_infomace(token):
     da_class = json.loads(r_class.text)
     msg = da_class['state']
     if msg == 'ok':
+        code = 0
         class_data = da_class['data']
         for class_umb in class_data:
             class_name_list.append(class_umb['className'])
@@ -52,11 +53,11 @@ def class_infomace(token):
             subject = da_subject['data']
             subject = subject[0]
             class_subject_list.append(subject['sid'])
-        return class_name_list,class_id_list,class_subject_list
+        return code,class_name_list,class_id_list,class_subject_list
     else:
-        code = 401
+        code = 2
         class_name_list = class_id_list = class_subject_list = None
-        return class_name_list,class_id_list,class_subject_list
+        return code,class_name_list,class_id_list,class_subject_list
 
 def homework_list_infomance(token,uid,class_id,subject_id):
     url = data_json['url_homework_list']
@@ -71,16 +72,12 @@ def homework_list_infomance(token,uid,class_id,subject_id):
     da_homework_list = json.loads(r_homework_list.text)
     msg = da_homework_list['state']
     if msg == 'ok':
+        code = 0
         for homework_umb in da_homework_list['data']:
-            code = 200
             homework_list_name_list.append(homework_umb['taskName'])
             homework_list_hid_list.append(homework_umb['taskId'])
         return code,homework_list_name_list,homework_list_hid_list
     else:
-        code = 404
+        code = 3
         homework_list_name_list = homework_list_hid_list = None
         return code,homework_list_name_list,homework_list_hid_list
-
-
-
-
